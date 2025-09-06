48c84fc8680d5baa71947ad59b2c30934ec40cb8
etolgademirel <228428243+etolgademirel@users.noreply.github.com>
2025-09-06 12:02:23 +0300

    Fix indenting

--- .config/nvim/lua/atom/plugins/treesitter.lua
+++ .config/nvim/lua/atom/plugins/treesitter.lua
@@ -1,39 +1,39 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  event = { "BufReadPre", "BufNewFile" },
-  build = ":TSUpdate",
-  config = function()
-    -- import nvim-treesitter plugin
-    local treesitter = require("nvim-treesitter.configs")
+    "nvim-treesitter/nvim-treesitter",
+    event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    config = function()
+        -- import nvim-treesitter plugin
+        local treesitter = require("nvim-treesitter.configs")
 
-    -- configure treesitter
-    treesitter.setup({ -- enable syntax highlighting
-      highlight = {
-        enable = true,
-      },
-      -- enable indentation
-      indent = { enable = true },
-      -- ensure these language parsers are installed
-      ensure_installed = {
-        "json",
-        "bash",
-        "lua",
-        "vim",
-        "gitignore",
-        "c",
-      },
-      incremental_selection = {
-        enable = true,
-        keymaps = {
-          init_selection = "<C-space>",
-          node_incremental = "<C-space>",
-          scope_incremental = false,
-          node_decremental = "<bs>",
-        },
-      },
-    })
+        -- configure treesitter
+        treesitter.setup({ -- enable syntax highlighting
+            highlight = {
+                enable = true,
+            },
+            -- enable indentation
+            indent = { enable = true },
+            -- ensure these language parsers are installed
+            ensure_installed = {
+                "json",
+                "bash",
+                "lua",
+                "vim",
+                "gitignore",
+                "c",
+            },
+            incremental_selection = {
+                enable = true,
+                keymaps = {
+                    init_selection = "<C-space>",
+                    node_incremental = "<C-space>",
+                    scope_incremental = false,
+                    node_decremental = "<bs>",
+                },
+            },
+        })
 
-    -- use bash parser for zsh files
-    vim.treesitter.language.register("bash", "zsh")
-  end,
+        -- use bash parser for zsh files
+        vim.treesitter.language.register("bash", "zsh")
+    end,
 }
