cec722057738544e5a5f483865c1c4e2b0b9c67b
etolgademirel <228428243+etolgademirel@users.noreply.github.com>
2025-09-06 12:03:39 +0300

    Add new keymaps

--- .config/nvim/lua/atom/core/keymaps.lua
+++ .config/nvim/lua/atom/core/keymaps.lua
@@ -4,7 +4,7 @@ local keymap = vim.keymap
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 
-keymap.set("n", "<leader>ch", ":nohl<CR>", { desc = "Clear search highlights" })
+keymap.set("n", "<leader>ch", ":nohl<cr>", { desc = "Clear search highlights" })
 
 keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" })
 keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
@@ -12,11 +12,16 @@ keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" })
 keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split vertically" })
 keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split horizontally" })
 keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close focused split" })
+keymap.set("n", "<leader>sx", "<cmd>close<cr>", { desc = "Close focused split" })
 keymap.set("n", "<leader>sn", "<C-w>w", { desc = "Move to next split" })
 
-keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" })
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" })
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" })
-keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" })
+keymap.set("n", "<leader>to", "<cmd>tabnew<cr>", { desc = "Open new tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<cr>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<cr>", { desc = "Go to next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<cr>", { desc = "Go to previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<cr>", { desc = "Open current buffer in new tab" })
+
+keymap.set("n", "<C-s>", "<cmd>w<cr>", { desc = "Save current file" })
+keymap.set("n", "<leader>q", "<cmd>q!<cr>", { desc = "Close current file" })
+keymap.set("n", "<leader>qq", "<cmd>qa!<cr>", { desc = "Close everything" })
+
